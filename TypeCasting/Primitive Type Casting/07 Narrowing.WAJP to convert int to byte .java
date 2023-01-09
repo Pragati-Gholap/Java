d85cd8811264e7795f983039dd1186eb50281664
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 22:55:22 +0530

    Add files via upload

---
+++ TypeCasting/Primitive Type Casting/07 Narrowing.WAJP to convert int to byte .java
@@ -0,0 +1,13 @@
+//07 Narrowing.WAJP to convert int to byte.
+public class Narrowing2 {
+
+	public static void main(String[] args) {
+		int a=130;
+		byte b=(byte)a;
+		System.out.println(a);//130
+		System.out.println(b);//-126
+		
+
+	}
+
+}
