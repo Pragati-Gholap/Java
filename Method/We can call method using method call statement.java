13558d9758e36a65441a944a732d4095a2dc59fb
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 23:24:03 +0530

    Add files via upload

---
+++ Method/We can call method using method call statement.java
@@ -0,0 +1,16 @@
+//We can call method using method call statement.
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("from main()");
+		m1(); //method call statement
+		System.out.println("End of main()");
+	}
+	public static void m1()
+	{
+		System.out.println("from m1()");
+		
+
+	}
+
+}
