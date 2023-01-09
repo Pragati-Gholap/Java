54fd59b436ccf99509b6b80eec2f38bab8276b70
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 22:52:13 +0530

    Add files via upload

---
+++ TypeCasting/Primitive Type Casting/02.Widening WAJP to convert character to int.java
@@ -0,0 +1,15 @@
+//wAJP To Convert Character to int.
+public class Program 
+	{
+
+	public static void main(String[] args)
+		{
+		char ch='A';
+		int a=ch;  //widening
+		System.out.println(a); //65
+
+	    }
+
+}
+
+
