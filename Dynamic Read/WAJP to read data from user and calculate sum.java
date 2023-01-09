15f8471545cd4e58a302c6d7734297f12e1314da
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 23:12:22 +0530

    Add files via upload

---
+++ Dynamic Read/WAJP to read data from user and calculate sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//WAJP to read data from user and calculate sum.
+public class Dynamic1 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter first value");
+		int a=s.nextInt();
+		System.out.println("Enter second value");
+		int b=s.nextInt();
+		int sum=a+b;
+		System.out.println(sum);
+		
+
+	}
+
+}
