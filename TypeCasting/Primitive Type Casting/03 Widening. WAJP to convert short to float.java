0145e9eb7228d5c832f3c1f10733c605b7db9092
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 22:52:48 +0530

    Add files via upload

---
+++ TypeCasting/Primitive Type Casting/03 Widening. WAJP to convert short to float.java
@@ -0,0 +1,15 @@
+//02 Widening. WAJP to convert short to float
+public class Program1 {
+
+public static void main(String[] args) {
+	short s=10;
+	float f=s;
+	System.out.println(f);//10.0
+	
+
+	}
+
+}
+
+
+
