9cf078b5289777628659c3507bc0d65152d25456
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 23:11:14 +0530

    Add files via upload

---
+++ Dynamic Read/WAJP to read employee name,employee id,and employee salary and print it.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//WAJP to read employee name,employee id,and employee salary and print it
+public class Dynamic2 {
+
+	public static void main(String[] args) {
+	 Scanner s=new Scanner(System.in);
+	 System.out.println("Enter employee name");
+	 String a=s.next();
+	 System.out.println("Enter employee id");
+	 int b=s.nextInt();
+	 System.out.println("Enter employee salary");
+	 double c=s.nextDouble();
+	 System.out.println(a);
+	 System.out.println(b);
+	 System.out.println(c);
+
+	}
+
+}
