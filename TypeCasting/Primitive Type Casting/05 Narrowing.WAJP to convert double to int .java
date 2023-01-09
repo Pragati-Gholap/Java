ef92c088aafd8c7e094b116217934a95bac0107e
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 22:54:03 +0530

    Add files via upload

---
+++ TypeCasting/Primitive Type Casting/05 Narrowing.WAJP to convert double to int .java
@@ -0,0 +1,12 @@
+//05 Narrowing.WAJP to convert double to int.
+public class Narrowing {
+
+	public static void main(String[] args) {
+	 double d=24;
+	 int a=(int)d; //narrowing
+	 System.out.println(d);//24.0
+	 System.out.println(a);//24
+
+	}
+
+}
