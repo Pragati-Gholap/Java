e371396f48a13b5534b1eba339362b9eb5f39c8a
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 22:56:08 +0530

    Add files via upload

---
+++ TypeCasting/Primitive Type Casting/08 Narrowing WAJP to convert double to byte,short,int,char.java
@@ -0,0 +1,19 @@
+//08 Narrowing WAJP to convert double to byte,short,int,char.
+public class Narrowing3 {
+
+	public static void main(String[] args) {
+		double d=67.34;
+		byte b=(byte)d;
+		short s=(byte)d;
+		int i=(int)d;
+		char ch=(char)d;
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(ch);
+	
+
+	}
+
+}
