ddd64c1513e7ce37b3b97afb4c26ac06437eb205
PragatiGholap <pragatigholap2@gmail.com>
2023-01-09 22:53:31 +0530

    Add files via upload

---
+++ TypeCasting/Primitive Type Casting/04 Widening .WAJP to convert boolean to int and boolean to double..java
@@ -0,0 +1,15 @@
+// 03 Widening .WAJP to convert boolean to int and boolean to double.
+public class Program2 {
+
+	public static void main(String[] args) {
+	boolean b=true;
+	int a=b; //compile time error
+	double d=b; //compile time error
+	System.out.println(a);
+	System.out.println(d);
+	
+
+	}
+
+}
+
