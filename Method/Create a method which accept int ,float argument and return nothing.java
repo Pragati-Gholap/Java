51058940654fa61c9b60b430f0646d4064bb60a3
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:04:50 +0530

    Add files via upload

---
+++ Method/Create a method which accept int ,float argument and return nothing.java
@@ -0,0 +1,14 @@
+//Create a method which accept int ,float argument and return nothing
+public class Demo6 {
+
+	public static void main(String[] args) {
+		m1(10,10.9f);
+
+	}
+	public static void m1(int a,float b)
+	{
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
