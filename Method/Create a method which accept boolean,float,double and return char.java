c2aa6e5bea770e076a8ef2fe66bdb88df754213c
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:04:03 +0530

    Add files via upload

---
+++ Method/Create a method which accept boolean,float,double and return char.java
@@ -0,0 +1,14 @@
+//Create a method which accept boolean,float,double and return char.
+public class Demo11 {
+
+	public static void main(String[] args) {
+		char ch=m1(true,3.9f,23.40);
+		System.out.println(ch);
+
+	}
+	public static char m1(boolean a,float b,double c)
+	{
+		return 'd';
+	}
+
+}
