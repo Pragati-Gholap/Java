621c9eca544f1e90e32c62795b7d8c3a1f372e0a
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:11:18 +0530

    Add files via upload

---
+++ Static members of class/accessing static variable directly with its name from the class.java
@@ -0,0 +1,14 @@
+package staticvariable;
+//accessing static variable directly with its name from the class
+ public class A {
+   static int k;
+	
+   public static void main(String[] args)
+   {
+    	  System.out.println();
+    	  
+	}
+
+}
+
+
