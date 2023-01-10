4bab7ea6eeb2e1078dbd5337620a11cd777d6da2
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:03:40 +0530

    Add files via upload

---
+++ Method/Create a method that should accept two int values and return nothing,and print that sum of two number.java
@@ -0,0 +1,11 @@
+//Create a method that should accept two int values and return nothing,and print that sum of two number
+public class Demo7 {
+
+	public static void main(String[] args) {
+		m1(10,20);
+	}
+	public static void m1(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+}
