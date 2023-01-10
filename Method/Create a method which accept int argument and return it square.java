61591d84b9cb4a2e3ea404685593cdd2c153abfe
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:05:17 +0530

    Add files via upload

---
+++ Method/Create a method which accept int argument and return it square.java
@@ -0,0 +1,13 @@
+//Create a method which accept int argument and return it square
+public class Demo9 {
+
+	public static void main(String[] args) {
+      System.out.println("square of number ="+m1(4));
+		}
+	public static int m1(int a)
+	{
+		int res=a*a;
+		return res;
+	}
+
+}
