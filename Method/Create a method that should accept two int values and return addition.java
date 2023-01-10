66e8511c7849e4d1ca1af005bffbcb1ddb5fc3a0
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:03:18 +0530

    Add files via upload

---
+++ Method/Create a method that should accept two int values and return addition.java
@@ -0,0 +1,14 @@
+//Create a method that should accept two int values and return addition.
+public class Demo4 {
+
+	public static void main(String[] args) {
+		int result=add(10,20);
+		System.out.println(result);
+		}
+	public static int add(int a,int b)
+	{
+		int c=a+b;
+		return c;
+	}
+
+}
