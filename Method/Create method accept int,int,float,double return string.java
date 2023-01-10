7162b41d930711c7994246468621f0cb18b37098
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:07:20 +0530

    Add files via upload

---
+++ Method/Create method accept int,int,float,double return string.java
@@ -0,0 +1,13 @@
+//Create method accept int,int,float,double return string.
+public class Demo12 {
+
+	public static void main(String[] args) {
+		System.out.println(m1(4,5,4.3f,40.30));
+	
+    }
+	public static String m1(int a,int b,float c,double d)
+	{
+		return "a";
+	}
+
+}
