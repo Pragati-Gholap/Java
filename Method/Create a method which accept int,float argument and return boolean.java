9cd0b85808c75e99f6d93208609bdd595c2cc820
PragatiGholap <pragatigholap2@gmail.com>
2023-01-10 13:06:47 +0530

    Add files via upload

---
+++ Method/Create a method which accept int,float argument and return boolean.java
@@ -0,0 +1,13 @@
+//Create a method which accept int,float argument and return boolean.
+ public class Demo10 {
+
+	public static void main(String[] args) {
+		boolean c=m1(4,4.3f);
+		System.out.println(c);
+		}
+	public static boolean m1(int a,float b)
+	{
+		return false;
+	}
+
+}
