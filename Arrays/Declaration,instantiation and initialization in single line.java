9e15c45378dbb3ab369db4cf955183bf96b8ea43
PragatiGholap <pragatigholap2@gmail.com>
2023-02-28 13:21:34 +0530

    Add files via upload

---
+++ Arrays/Declaration,instantiation and initialization in single line.java
@@ -0,0 +1,13 @@
+//Declaration,instantiation and initialization in single line.
+public class Array3 {
+
+	public static void main(String[] args) {
+		int[]a= {10,20,30,40,50};
+		for(int i=0;i<=a.length-1;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
