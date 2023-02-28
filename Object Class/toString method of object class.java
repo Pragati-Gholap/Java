d28fa252c9816b106e1dbe390f6370efdaa75731
PragatiGholap <pragatigholap2@gmail.com>
2023-02-28 12:49:35 +0530

    Add files via upload

---
+++ Object Class/toString method of object class.java
@@ -0,0 +1,15 @@
+//toString method of object class
+public class Demo {
+
+	public static void main(String[] args) {
+		Demo d1=new Demo();
+		//Whenever programmer try to print the reference
+		//variable of an object implicitly toString()of
+		//object class will be called 
+		System.out.println(d1);//---->d1.toString();
+		
+		
+
+	}
+
+}
